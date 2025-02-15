71c7d3ef4e51c843091106e9e4ea43a6b5104041
Thomas Hinds <thinds101@gmail.com>
2025-02-15 12:13:14 -0800

    Re-add wasm packed files

---
+++ docs/panopto-pkg/panopto_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const process: (a: number, b: number, c: number, d: number) => void;
+export const main: () => void;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
